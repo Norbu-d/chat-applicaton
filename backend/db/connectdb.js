18aa4050e32f3ebe6f152c71a8a4253451dabc64
Norbu-d <02230293.cst@rub.edu.bt>
2024-09-09 01:09:55 +0600

    plzzz

--- backend/db/connectdb.js
+++ backend/db/connectdb.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const connectdb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
-        console.log("connected to mongodb"); 
-
-    }catch (error) {
-        console.log("Error connecting to Mongodb",error.messages)
-
+        await mongoose.connect(process.env.MONGO_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB"); 
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message || error);
     }
-}
+};
 
 export default connectdb;
